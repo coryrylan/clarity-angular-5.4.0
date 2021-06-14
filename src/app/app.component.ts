e439e85946c8bd3c55052877a9ab37f363cbbe3e
Cory Rylan <splintercode.cb@gmail.com>
2021-06-14 12:10:09 -0500

    chore: add Clarity 5.4.0
    Signed-off-by: Cory Rylan <splintercode.cb@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ClarityIcons, userIcon } from '@cds/core/icon';
+
+ClarityIcons.addIcons(userIcon);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-app';
+  showAlert = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ClarityModule } from '@clr/angular';
+import { CdsModule } from '@cds/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule,
+    CdsModule,
     AppRoutingModule
   ],
   providers: [],
